970ae35f563122d13ab4eeed3c3a172c1402fc0a
unknown <altabrakib7@gmail.com>
2023-01-28 14:08:48 +0600

    first commite

---
+++ js/index.js
@@ -0,0 +1,21 @@
+new WOW().init();
+
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  })
+    
+});
